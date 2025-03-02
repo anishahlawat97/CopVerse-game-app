65e1ef2cb144d14bc0ca553bc84bcaaff97b5e3c
Anish <anish.mi619@gmail.com>
2025-03-02 23:03:05 +0530

    layout updated with metadata and other minor updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import Navbar from "@/components/Navbar";
 import { cn } from "./utils/utils";
+import { Suspense } from "react";
+import Ripple from "@/components/magicui/ripple";
+import Head from "next/head";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -19,6 +22,7 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "CopVerse",
   description: "Capture the fugitive",
+  icons: 'copverse-logo-2.png',
 };
 
 export default function RootLayout({
@@ -28,13 +32,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={cn("bg-background text-foreground", geistSans.variable, geistMono.variable)}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="author" content="CopVerse" />
+        <link rel="icon" href="/copverse-logo-2.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <body
         className="antialiased"
-      >
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <Navbar />
-          {children}
-        </ThemeProvider>
+      > <Suspense fallback={<Ripple />}>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <Navbar />
+            {children}
+          </ThemeProvider>
+        </Suspense>
       </body>
     </html>
   );
